a6ee6e907898d29a94bbb8a096685380bbfb9dc6
MariaPaulaMM <75758318+MariaPaulaMM@users.noreply.github.com>
2020-12-11 12:05:11 -0500

    Name en vez de Username

--- dinerall_api/database/user_db.py
+++ dinerall_api/database/user_db.py
@@ -6,30 +6,32 @@ class UserInDB(BaseModel):
     name: str
     mail: str #pienso que es mejor por correo porque no le veo la finalidad a que los usuarios tengan un seudonimo dado que no tienen interaccion entre ellos, sin embargo, lo dejo por si algo sale mal
     password: str
-    birthday:
+    birthday: datetime.date
     gender: str
 
 database_users = Dict[str, UserInDB]
 database_users = {
-    "Carlos": UserInDB(**{"username":"Carlos",
+    "Carlos": UserInDB(**{"name":"Carlos",
                             "mail":"carlos@gmail.com",
                             "password":"carlos12345"}),
-    "Veronica": UserInDB(**{"username":"Veronica",
+    "Veronica": UserInDB(**{"name":"Veronica",
                             "mail":"veronica@gmail.com",
                             "password":"veronica12345"}),
-    "James": UserInDB(**{"username":"James",
+    "James": UserInDB(**{"name":"James",
                             "mail":"james@gmail.com",
                             "password":"james12345"}),
 }
 #funcion de loguear
-def login(username:str,password:str):
-    if username and password in database_users.keys():
-        return "Se ha logueado correctamente. Bienvenido a Dinerall "+database_users[username]+"."
+def login(mail:str,password:str):
+    if mail and password in database_users.keys():
+        return "Se ha logueado correctamente. Bienvenido a Dinerall "+database_users[mail]+"."
     else:
         return "Usuario o contraseña incorrectos. Pruebe nuevamente o regístrese."
 #funcion de registro, en esta no estoy muy seguro si esta bien, ya seria mirar en pruebas
-def signin(username:str,mail:str,password:str):
-    username=input[database_users[username]]
+def signup(name:str,mail:str,password:str, birthday:datetime.date,gender:str):
+    name=input[database_users[name]]
     mail=input[database_users[mail]]
     password=input[database_users[password]]
+    birthday=input[database_users[birthday]]
+    gender=input[database_users[gender]]
     return "Usuario registrado correctamente."
--- dinerall_api/models/user_models.py
+++ dinerall_api/models/user_models.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
 
 class UserIn(BaseModel):
-    username:str
+    name:str
     mail: str
     password:str
     
 
 class UserOut(BaseModel):
-    username:str
+    name:str
