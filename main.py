d1f7d68f2394a68cd8393a86d2365c3271e72418
Seigan94 <60443191+Seigan94@users.noreply.github.com>
2020-12-15 09:45:40 -0500

    Front-Back

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ import datetime
 from database.user_db import UserInDB
 from database.user_db import get_user, save_user
 from models.user_models import UserIn, UserNew, UserOut
+from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
+
+
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+    "https://dinerall.herokuapp.com/" 
+]
+
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
 
 @api.get("/")
 async def read_root():
